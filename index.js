a2e9c5677319947a0399b2be547418d6889691db
Mitch Denny <mitch@mitchdenny.com>
2015-07-25 20:41:36 +1000

    Playing around with the architecture.

--- index.js
+++ index.js
@@ -1,7 +1,58 @@
+var Context = function() {
+  var self = this;
+
+  return {
+    writeItem: function(item) {
+
+    }
+  };
+};
+
+var Command = function(verb, noun, itemProcessor) {
+  var self = this;
+  self.verb = verb;
+  self.noun = noun;
+  self.itemProcessor = itemProcessor;
+
+  return {
+    process: function(context, item) {
+      self.itemProcessor(context, item);
+    }
+  };
+};
+
+var Extension = function(module) {
+  var self = this;
+  self.commands = [];
+
+  return {
+    addCommand: function(verb, noun, itemProcessor) {
+      var command = new Command(verb, noun, itemProcessor);
+      self.commands.push(command);
+      return command;
+    },
+    getCommands: function() {
+      return self.commands;
+    }
+  };
+};
+
 var Chewy = function() {
   var self = this;
 
-  return {};
+  return {
+    extend: function(module) {
+      var extension = new Extension(module);
+      return extension;
+    }
+  };
 }
 
-module.exports = new Chewy();
+var chewy = new Chewy();
+var extension = chewy.extend(chewy);
+
+extension.addCommand('get', 'command', function(context, item) {
+
+});
+
+module.exports = chewy;
